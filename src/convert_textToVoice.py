1b8cffa6a20b55cfa952ed394598e8bd615be8c6
rafael-torres-nantes <rafatorresnantes@gmail.com>
2023-10-19 02:20:33 -0400

    TTS usingPyTTSx3

--- src/convert_textToVoice.py
+++ src/convert_textToVoice.py
@@ -1,9 +1,38 @@
 import pyttsx3
 
-def textToSpeech(textFile):
+def textToSpeech(textFile, audioSaveOn=False, audioName='output_audio.wav', audioReproduceOn=False, volume=0.5):
 
-    textToSpeech = pyttsx3.init()
+    # Object creation
+    reproducerText = pyttsx3.init()
+    volume = getVolume(reproducerText)
 
-    textToSpeech.say(textFile)
-    textToSpeech.runAndWait()
+    directory = 'audio_folder/'
+    file_path = directory + audioName
 
+    if volume != 0.5:
+        volumeChange(reproducerText, volume)
+        volume = getVolume(reproducerText)
+    
+    if audioSaveOn == True:
+        saveAudio(reproducerText, textFile)
+
+    if audioReproduceOn == True:
+        reproduceAudio(reproducerText, textFile)
+
+def reproduceAudio(reproducerText, textFile):
+    reproducerText.say(textFile)
+    reproducerText.runAndWait()
+
+def saveAudio(reproducerText, textFile, file_path):
+    reproducerText.save_to_file(textFile, file_path)
+    reproducerText.runAndWait()
+
+def getVolume(reproducerText):
+    # Retorna o volume atual
+    return reproducerText.getProperty('volume')
+
+def volumeChange(reproducerText, volume):
+    # min = 0 and max = 1.0
+    reproducerText.setProperty('volume', volume)
+    print(f'O volume passou a ser: [{volume * 100}%]')
+    return reproducerText
